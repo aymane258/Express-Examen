4cf573d1e878d43bd943dec0c87ce19c9ac0bf05
aymane258 <aymane-abdellah@hotmail.com>
2020-01-20 11:05:08 +0100

    initial commit
    adding mongodb database

---
+++ app/routes/students.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+var db;
+
+MongoClient.connect('mongodb://localhost:27017', (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('exam')
+})
+
+
+/* GET ALL PRODUCTS */
+router.get('/', (req, res) => {
+  db.collection('exam').find().toArray((err,result) => {
+      console.log(result[0].naam)
+    if (err) return
+    res.render('list',{students:result})
+  })
+})
+
+/* SHOW ADD PRODUCT FORM */
+router.get('/add', (req, res) => {
+  res.render('add.ejs', {})
+})
+  
+  router.post('/add',(req,res)=> {
+
+    db.collection('students').find(req.body).toArray((err,result)=> {
+      if(err) return console.log(err)
+      if(result != '') {
+      res.render('error') 
+      console.log(result)
+    }
+      else {
+      db.collection('students').insertOne({
+     naam:req.body.naam,
+      geboortedatum:new Date().toLocaleDateString(),
+      studierichting: req.body.studierichting
+    }
+      )
+        res.redirect('/')
+    }}) 
+    
+  })
+
+
+module.exports = router;
